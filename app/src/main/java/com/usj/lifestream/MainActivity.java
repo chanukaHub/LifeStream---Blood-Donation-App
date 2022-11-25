44f55bddd6bc849e22e23d7fe19a717406d304ba
chanukaHub <chanukadil1999@gmail.com>
2022-11-26 00:36:37 +0530

    add animation to ss screen

--- app/src/main/java/com/usj/lifestream/MainActivity.java
+++ app/src/main/java/com/usj/lifestream/MainActivity.java
@@ -6,16 +6,34 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    private static  int SPLASH_SCREEN = 2000;
+    private static  int SPLASH_SCREEN = 2500;
+
+    ImageView imageView;
+    TextView textView1, textView2;
+    Animation top, bottom;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
 
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        imageView = findViewById(R.id.imageView);
+        textView1 = findViewById(R.id.app_name);
+        textView2 = findViewById(R.id.sub_topic);
+
+
+        top = AnimationUtils.loadAnimation(this, R.anim.top);
+        bottom = AnimationUtils.loadAnimation(this, R.anim.bottom);
+        imageView.setAnimation(top);
+        textView1.setAnimation(bottom);
+        textView2.setAnimation(bottom);
 
         new Handler().postDelayed(new Runnable() {
             @Override
