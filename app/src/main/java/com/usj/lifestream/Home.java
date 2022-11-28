c8c09b574dc9539bbc398275a398478490ee2f44
chanukaHub <chanukadil1999@gmail.com>
2022-11-28 21:29:44 +0530

    added side nav ui

--- app/src/main/java/com/usj/lifestream/Home.java
+++ app/src/main/java/com/usj/lifestream/Home.java
@@ -1,14 +1,26 @@
 package com.usj.lifestream;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class Home extends AppCompatActivity {
+    private FirebaseUser user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        Toolbar toolbar =findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        toolbar.setNavigationIcon(R.drawable.menu);
+
+        user= FirebaseAuth.getInstance().getCurrentUser();
     }
 }
