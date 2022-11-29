2cedacd97de68fa8899a0cfcc2c7417890d07479
rukshini <rukshinigomis@gmail.com>
2022-11-29 14:38:49 +0530

    added fragments

---
+++ app/src/main/java/com/usj/lifestream/fragments/EventFragment.java
@@ -0,0 +1,21 @@
+package com.usj.lifestream.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.usj.lifestream.R;
+
+public class EventFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_event, container, false);
+    }
+}
